0ef500de9f98290b526554eb5f0c69cb777d4c89
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2019-10-08 23:41:08 +0900

    :wrench: show sources from firestore

--- components/CheckoutForm.js
+++ components/CheckoutForm.js
@@ -6,7 +6,35 @@ import { firestore } from "../lib/firebase";
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      sources: []
+    };
     this.submit = this.submit.bind(this);
+  }
+  componentDidMount() {
+    firestore
+      .collection("stripe_customers")
+      .doc(this.props.currentUser.uid)
+      .collection("sources")
+      .onSnapshot(
+        snapshot => {
+          let newSources = [];
+          snapshot.forEach(doc => {
+            const id = doc.id;
+            newSources.push({ key: id, value: doc.data() });
+          });
+          const state = Object.assign(this.state, {
+            sources: newSources
+          });
+          this.setState(state);
+        },
+        () => {
+          const state = Object.assign(this.state, {
+            sources: []
+          });
+          this.setState(state);
+        }
+      );
   }
 
   async submit(ev) {
@@ -23,11 +51,20 @@ class CheckoutForm extends Component {
 
   render() {
     return (
-      <div className="checkout">
-        <p>Would you like to complete the purchase?</p>
-        <CardElement onReady={c => (this._element = c)} />
-        <button onClick={this.submit}>Purchase</button>
-      </div>
+      <>
+        <div className="checkout">
+          <p>Would you like to complete the purchase?</p>
+          <CardElement onReady={c => (this._element = c)} />
+          <button onClick={this.submit}>Purchase</button>
+        </div>
+        <div>
+          <ul>
+            {this.state.sources.map(source => {
+              return <li key={source.key}>{source.value.last4}</li>;
+            })}
+          </ul>
+        </div>
+      </>
     );
   }
 }
