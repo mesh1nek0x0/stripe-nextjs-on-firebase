71b556219421f451cc5b694dcb312e5b0916dcb8
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2019-10-14 16:51:38 +0900

    :wrench: charge in firebase/functions

--- functions/index.js
+++ functions/index.js
@@ -46,6 +46,42 @@ exports.addPaymentSource = functions.firestore
     }
   });
 
+exports.createStripeCharge = functions.firestore
+  .document("stripe_customers/{userId}/charges/{id}")
+  .onCreate(async (snap, context) => {
+    const val = snap.data();
+    try {
+      // Look up the Stripe customer id written in createStripeCustomer
+      const snapshot = await admin
+        .firestore()
+        .collection(`stripe_customers`)
+        .doc(context.params.userId)
+        .get();
+      const snapval = snapshot.data();
+      const customer = snapval.customer_id;
+      // Create a charge using the pushId as the idempotency key
+      // protecting against double charges
+      const amount = val.amount;
+      const idempotencyKey = context.params.id;
+      const charge = { amount, currency, customer };
+      if (val.source !== null) {
+        charge.source = val.source;
+      }
+      const response = await stripe.charges.create(charge, {
+        idempotency_key: idempotencyKey
+      });
+      // If the result is successful, write it back to the database
+      return snap.ref.set(response, { merge: true });
+    } catch (error) {
+      // We want to capture errors and render them in a user-friendly way, while
+      // still logging an exception with StackDriver
+      console.log(error);
+      await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
+      console.error(error);
+      console.log(`user: ${context.params.userId}`);
+    }
+  });
+
 function userFacingMessage(error) {
   return error.type
     ? error.message
